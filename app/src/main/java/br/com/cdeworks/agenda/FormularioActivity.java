aaa96f68b1eb73c9d978905e230458f48cdf89c9
Daniel <danielcdeworks@gmail.com>
2017-10-11 14:20:09 -0300

    Comit inicial

--- app/src/main/java/br/com/cdeworks/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/cdeworks/agenda/FormularioActivity.java
@@ -3,27 +3,45 @@ package br.com.cdeworks.agenda;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import br.com.cdeworks.agenda.modelo.Aluno;
+
 public class FormularioActivity extends AppCompatActivity {
+
+    private FormularioHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+        helper = new FormularioHelper (this);
+    }
 
-        Button botaoSalvar = (Button) findViewById(R.id.formulario_salvar);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
 
-        botaoSalvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(FormularioActivity.this, "Aluno salvo!", Toast.LENGTH_SHORT).show();
+        inflater.inflate(R.menu.menu_formulario, menu);
 
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_formulario_ok:
+                Aluno aluno = helper.getAluno();
+                Toast.makeText(FormularioActivity.this, "Aluno " + aluno.getNome() + " salvo!", Toast.LENGTH_SHORT).show();
                 finish();
-            }
-        });
+                break;
+        }
 
+        return super.onOptionsItemSelected(item);
     }
 }
